6547856eee1d4c5a7642f6fa729acbe28d615e48
samitrimal <rimalsamit@yahoo.com>
2011-08-29 20:15:58 +0530

    cms module

---
+++ base/classes/controller/admin/base.php
@@ -0,0 +1,42 @@
+<?php
+namespace Base;
+/**
+ * Description of base
+ *
+ * @author Your Name <your.name >
+ */
+class Controller_Admin_Base extends \Controller_Template{
+   //put your code here
+      public $template = 'admin/admin_template';
+    public $auto_render = true;
+    public static $instance='';
+  function before(){
+  
+      parent::before();
+       
+  }
+  public static function check_authuser(){
+      if (!\Auth\Auth::check()) {
+                \Auth\Auth::logout();
+                \Response::redirect('login/loginform');
+            }
+  }
+ 
+    public static function instance(){
+       if(!self::$instance){
+           self::$instance= new \Base\Controller_Base();
+       }
+     
+          return self::$instance;
+       
+    }
+      public static function load_template($var=''){
+                     return'admin/admin_template';
+     
+     
+         
+    }
+}
+
+
+?>
---
+++ base/classes/controller/base.php
@@ -0,0 +1,44 @@
+<?php
+namespace Base;
+/**
+ * Description of base
+ *
+ * @author Your Name <your.name >
+ */
+class Controller_Base extends \Controller_Template{
+   //put your code here
+      public $template = 'client/client_template';
+    public $auto_render = true;
+    public static $instance='';
+  function before(){
+  
+      parent::before();
+       
+  }
+  public static function check_authuser(){
+      if (!\Auth\Auth::check()) {
+                \Auth\Auth::logout();
+                \Response::redirect('login/loginform');
+            }
+  }
+ 
+    public static function instance(){
+       if(!self::$instance){
+           self::$instance= new \Base\Controller_Base();
+       }
+     
+          return self::$instance;
+       
+    }
+      public static function load_template($var=''){
+                     return'admin/admin_template';
+      }               
+       
+      function action_404(){
+        $this->template->title="404 Page Not Found";
+        $this->template->content=View::factory('base/404.php');
+      }
+}
+
+
+?>
